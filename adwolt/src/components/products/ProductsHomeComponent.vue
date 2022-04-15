05f6e081727a56bd3b52e1d6b305ad4e8f7c822c
Bhupinder <Bhupi@Harveens-MacBook-Pro.local>
2022-04-15 16:31:25 +0530

    Products list

--- adwolt/src/components/products/ProductsHomeComponent.vue
+++ adwolt/src/components/products/ProductsHomeComponent.vue
@@ -5,9 +5,13 @@
       <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
           <div class="card h-100">
-            <img src="" class="card-img-top" alt="" />
+            <img
+              src="../../assets/adwolt-housing-wires.jpeg"
+              class="card-img-top"
+              alt="../../assets/adwolt-housing-wires.jpeg"
+            />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">House wires</h5>
               <p class="card-text">
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
@@ -18,9 +22,13 @@
         </div>
         <div class="col">
           <div class="card h-100">
-            <img src="" class="card-img-top" alt="" />
+            <img
+              src="../../assets/adwolt-submersible-cables.png"
+              class="card-img-top"
+              alt=""
+            />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Submersible wires</h5>
               <p class="card-text">
                 This card has supporting text below as a natural lead-in to
                 additional content.
@@ -30,9 +38,64 @@
         </div>
         <div class="col">
           <div class="card h-100">
-            <img src="" class="card-img-top" alt="" />
+            <img
+              src="../../assets/adwolt-power-cable.jpeg"
+              class="card-img-top"
+              alt=""
+            />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Multicore wires</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img
+              src="../../assets/adwolt-coaxial-cable-large.jpeg"
+              class="card-img-top"
+              alt=""
+            />
+            <div class="card-body">
+              <h5 class="card-title">Coaxial wires</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img
+              src="../../assets/adwolt-service-wire.jpeg"
+              class="card-img-top"
+              alt=""
+            />
+            <div class="card-body">
+              <h5 class="card-title">Service wires</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img
+              src="../../assets/LAN-cable.jpeg"
+              class="card-img-top"
+              alt=""
+            />
+            <div class="card-body">
+              <h5 class="card-title">LAN cable</h5>
               <p class="card-text">
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This card has even longer content
@@ -48,3 +111,9 @@
 <script>
 export default {};
 </script>
+
+<style scoped>
+div.card > img {
+  max-height: 250px !important;
+}
+</style>
