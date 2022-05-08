c06e655e83e55097c958aecfe6e0f4ffd38536bf
Bhupinder <Bhupi@Harveens-MacBook-Pro.local>
2022-05-08 19:55:14 +0530

    Add new images carousel

--- adwolt/src/components/home/CarouselComponent.vue
+++ adwolt/src/components/home/CarouselComponent.vue
@@ -9,6 +9,20 @@
         aria-current="true"
         aria-label="Slide 1"
       ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselComponent"
+        data-bs-slide-to="1"
+        aria-current="true"
+        aria-label="Slide 2"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselComponent"
+        data-bs-slide-to="2"
+        aria-current="true"
+        aria-label="Slide 3"
+      ></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -16,6 +30,22 @@
           src="../../assets/carousel-1.png"
           class="d-block w-100"
           alt="../../assets/carousel-1.png"
+        />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="../../assets/carousel-2.png"
+          class="d-block w-100"
+          style="height: 502px"
+          alt="../../assets/carousel-2.png"
+        />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="../../assets/carousel-3.png"
+          class="d-block w-100"
+          style="height: 502px"
+          alt="../../assets/carousel-3.png"
         />
       </div>
     </div>
