2059ae995cb8b0bd19cba3d61bd06c3e9e1744e6
Bhupinder <Bhupi@Harveens-MacBook-Pro.local>
2022-05-08 14:13:23 +0530

    Add content in footer

--- adwolt/src/components/FooterComponent.vue
+++ adwolt/src/components/FooterComponent.vue
@@ -21,7 +21,9 @@
               </a>
             </div>
           </div>
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div
+            class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-lg-block"
+          >
             <h6 class="text-uppercase fw-bold mb-4">Products</h6>
             <p>
               <a href="#!" class="text-reset">Adwolt wires</a>
@@ -36,7 +38,9 @@
               <a href="#!" class="text-reset">some other product</a>
             </p>
           </div>
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div
+            class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-lg-block"
+          >
             <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
             <p>
               <a href="/" class="text-reset">Home</a>
@@ -53,13 +57,15 @@
           </div>
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3"></i>Amritsar</p>
+            <p>
+              <i class="fas fa-home me-3"></i>B-2A, East Mohan Nagar, Amritsar
+            </p>
             <p>
               <i class="fas fa-envelope me-3"></i>
-              info@example.com
+              angaddistributors@gmail.com
             </p>
-            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            <p><i class="fas fa-phone me-3"></i>+91 99888 79333</p>
+            <p><i class="fas fa-print me-3"></i>+91 99889 79333</p>
           </div>
         </div>
       </div>
